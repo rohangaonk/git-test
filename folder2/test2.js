4a180fe1aa22fd2245cdfe3a073aeb8972d323ec
rohan <gaonkar70@gmail.com>
2022-03-20 17:52:56 +0530

    reverted back to min func commit

--- folder2/test2.js
+++ folder2/test2.js
@@ -5,15 +5,3 @@ function getMax(arr) {
 function getMin(arr) {
   return Math.min.apply(null, arr);
 }
-
-function getMid(arr) {
-  return arr[arr.length / 2];
-}
-
-function getFirst(arr) {
-  return arr[0];
-}
-
-function getLast(arr) {
-  return arr[arr.length - 1];
-}
